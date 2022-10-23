b88b200c363e625743f0d35e46debf58a72cd405
Ruslan Shakirov <ruslanshakir@gmail.com>
2022-10-23 16:30:44 +0300

    deploy

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import { y, d as delegateEvents } from "./vendor.js";
 const Footer = "";
 const Button = "";
 const Nav = "";
-function script$4() {
+function script$6() {
   const el = Array.from(document.querySelectorAll(".nav__sub"));
   if (!el)
     return;
@@ -18,10 +18,10 @@ function script$4() {
   });
 }
 {
-  script$4();
+  script$6();
 }
 const Hamburger = "";
-function script$3() {
+function script$5() {
   const el = document.querySelector(".hamburger");
   if (!el)
     return;
@@ -30,40 +30,43 @@ function script$3() {
   });
 }
 {
-  script$3();
+  script$5();
 }
 const Search = "";
-function script$2() {
+function script$4() {
   const el = document.querySelector(".search");
   if (!el)
     return;
   const closeEl = el.querySelector(".search__close");
+  function open() {
+    el.classList.add("search_open");
+  }
   function close() {
     el.classList.remove("search_open");
   }
   closeEl.addEventListener("click", close);
-  document.addEventListener("click", (e) => {
+  window.addEventListener("open-search", open);
+  window.addEventListener("click", (e) => {
     if (!el.contains(e.target))
       close();
   });
 }
 {
-  script$2();
+  script$4();
 }
 const Header = "";
-function script$1() {
+function script$3() {
   const el = document.querySelector(".header");
   if (!el)
     return;
   const searchButtonEl = el.querySelector(".header__search-button");
-  const searchEl = document.querySelector(".search");
   searchButtonEl.addEventListener("click", (e) => {
     e.stopPropagation();
-    searchEl.classList.add("search_open");
+    window.dispatchEvent(new CustomEvent("open-search"));
   });
 }
 {
-  script$1();
+  script$3();
 }
 const Banners = "";
 const Brands = "";
@@ -71,7 +74,7 @@ const Categories = "";
 const Features = "";
 const keenSlider_min = "";
 const Intro = "";
-function script() {
+function script$2() {
   const el = document.querySelector(".intro");
   if (!el)
     return;
@@ -92,6 +95,67 @@ function script() {
   });
   prevEl.addEventListener("click", () => slider.prev());
   nextEl.addEventListener("click", () => slider.next());
+}
+{
+  script$2();
+}
+const Populars = "";
+const Product = "";
+const ProductSlider = "";
+function script$1() {
+  const el = document.querySelectorAll(".product-slider");
+  if (!el)
+    return;
+  el.forEach((node) => {
+    const prevEl = node.querySelector(".product-slider__nav_prev");
+    const nextEl = node.querySelector(".product-slider__nav_next");
+    const sliderEl = node.querySelector(".product-slider__slider");
+    const slider = new y(sliderEl, {
+      loop: true,
+      slides: {
+        perView: 1
+      },
+      breakpoints: {
+        "(min-width: 576px)": {
+          slides: {
+            perView: 2
+          }
+        },
+        "(min-width: 768px)": {
+          slides: {
+            perView: 3
+          }
+        },
+        "(min-width: 992px)": {
+          slides: {
+            perView: 4
+          }
+        }
+      }
+    });
+    prevEl.addEventListener("click", (e) => slider.prev());
+    nextEl.addEventListener("click", (e) => slider.next());
+  });
+}
+{
+  script$1();
+}
+function script() {
+  const el = document.querySelector(".populars");
+  if (!el)
+    return;
+  const tabEls = Array.from(el.querySelectorAll(".populars__tab"));
+  const panelEls = el.querySelectorAll(".populars__tabpanel");
+  tabEls.forEach((tabEl) => {
+    tabEl.addEventListener("click", (e) => {
+      const index = tabEls.indexOf(tabEl);
+      tabEls.forEach((n) => n.classList.remove("populars__tab_selected"));
+      panelEls.forEach((n) => n.classList.remove("populars__tabpanel_selected"));
+      tabEl.classList.add("populars__tab_selected");
+      panelEls[index].classList.add("populars__tabpanel_selected");
+      window.dispatchEvent(new CustomEvent("resize"));
+    });
+  });
 }
 {
   script();
