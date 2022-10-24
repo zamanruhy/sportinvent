29462d76d96147d33796271034c2659464df6526
Ruslan Shakirov <ruslanshakir@gmail.com>
2022-10-24 13:17:48 +0300

    deploy

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-import { y, d as delegateEvents } from "./vendor.js";
+import { y } from "./vendor.js";
+const base = "";
+const common = "";
 const Footer = "";
 const Button = "";
 const Nav = "";
-function script$6() {
+function script$9() {
   const el = Array.from(document.querySelectorAll(".nav__sub"));
   if (!el)
     return;
@@ -18,22 +20,27 @@ function script$6() {
   });
 }
 {
-  script$6();
+  script$9();
 }
 const Hamburger = "";
-function script$5() {
+function script$8() {
   const el = document.querySelector(".hamburger");
   if (!el)
     return;
+  let active = false;
   el.addEventListener("click", (e) => {
-    el.classList.toggle("hamburger_active");
+    active = !active;
+    el.classList.toggle("hamburger_active", active);
+    window.dispatchEvent(new CustomEvent("hamburger:active", {
+      detail: active
+    }));
   });
 }
 {
-  script$5();
+  script$8();
 }
 const Search = "";
-function script$4() {
+function script$7() {
   const el = document.querySelector(".search");
   if (!el)
     return;
@@ -52,10 +59,27 @@ function script$4() {
   });
 }
 {
-  script$4();
+  script$7();
 }
+const Drawer = "";
+function script$6() {
+  const el = document.querySelector(".drawer");
+  if (!el)
+    return;
+  window.addEventListener("hamburger:active", ({
+    detail: active
+  }) => {
+    el.classList.toggle("drawer_open", active);
+  });
+}
+{
+  script$6();
+}
+const Callback = "";
+const Checkbox = "";
+const Input = "";
 const Header = "";
-function script$3() {
+function script$5() {
   const el = document.querySelector(".header");
   if (!el)
     return;
@@ -64,9 +88,17 @@ function script$3() {
     e.stopPropagation();
     window.dispatchEvent(new CustomEvent("open-search"));
   });
+  const cbButtonEls = Array.from(el.querySelectorAll(".header__callback, .header__callback-mobile"));
+  const dialogEl = el.querySelector(".header__dialog");
+  cbButtonEls.forEach((btnEl) => {
+    btnEl.addEventListener("click", (e) => {
+      e.preventDefault();
+      dialogEl.showModal();
+    });
+  });
 }
 {
-  script$3();
+  script$5();
 }
 const Banners = "";
 const Brands = "";
@@ -74,7 +106,7 @@ const Categories = "";
 const Features = "";
 const keenSlider_min = "";
 const Intro = "";
-function script$2() {
+function script$4() {
   const el = document.querySelector(".intro");
   if (!el)
     return;
@@ -97,19 +129,19 @@ function script$2() {
   nextEl.addEventListener("click", () => slider.next());
 }
 {
-  script$2();
+  script$4();
 }
 const Populars = "";
 const Product = "";
 const ProductSlider = "";
-function script$1() {
-  const el = document.querySelectorAll(".product-slider");
-  if (!el)
+function script$3() {
+  const els = Array.from(document.querySelectorAll(".product-slider"));
+  if (!els.length)
     return;
-  el.forEach((node) => {
-    const prevEl = node.querySelector(".product-slider__nav_prev");
-    const nextEl = node.querySelector(".product-slider__nav_next");
-    const sliderEl = node.querySelector(".product-slider__slider");
+  els.forEach((el) => {
+    const prevEl = el.querySelector(".product-slider__nav_prev");
+    const nextEl = el.querySelector(".product-slider__nav_next");
+    const sliderEl = el.querySelector(".product-slider__slider");
     const slider = new y(sliderEl, {
       loop: true,
       slides: {
@@ -138,9 +170,9 @@ function script$1() {
   });
 }
 {
-  script$1();
+  script$3();
 }
-function script() {
+function script$2() {
   const el = document.querySelector(".populars");
   if (!el)
     return;
@@ -158,9 +190,62 @@ function script() {
   });
 }
 {
+  script$2();
+}
+const Breadcrumb = "";
+const Amount = "";
+function script$1() {
+  const els = Array.from(document.querySelectorAll(".amount"));
+  if (!els.length)
+    return;
+  els.forEach((el) => {
+    const decrEl = el.querySelector(".amount__button:first-of-type");
+    const incrEl = el.querySelector(".amount__button:last-of-type");
+    const inputEl = el.querySelector(".amount__input");
+    function set(val) {
+      if (val < 1)
+        val = 1;
+      decrEl.disabled = val < 2;
+      inputEl.value = val;
+    }
+    incrEl.addEventListener("click", (e) => set(+inputEl.value + 1));
+    decrEl.addEventListener("click", (e) => set(+inputEl.value - 1));
+  });
+}
+{
+  script$1();
+}
+const Card = "";
+function script() {
+  const el = document.querySelector(".card");
+  if (!el)
+    return;
+  const tabEls = Array.from(el.querySelectorAll(".card__tab"));
+  const panelEls = el.querySelectorAll(".card__tabpanel");
+  tabEls.forEach((tabEl) => {
+    tabEl.addEventListener("click", (e) => {
+      const index = tabEls.indexOf(tabEl);
+      tabEls.forEach((n) => n.classList.remove("card__tab_selected"));
+      panelEls.forEach((n) => n.classList.remove("card__tabpanel_selected"));
+      tabEl.classList.add("card__tab_selected");
+      panelEls[index].classList.add("card__tabpanel_selected");
+      window.dispatchEvent(new CustomEvent("resize"));
+    });
+  });
+  const pictureEl = el.querySelector(".card__picture");
+  const imgEl = el.querySelector(".card__img");
+  pictureEl.style.backgroundImage = `url(${imgEl.src})`;
+  pictureEl.addEventListener("mousemove", (e) => {
+    const zoomer = e.currentTarget;
+    let offsetX, offsetY;
+    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
+    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX;
+    const x = offsetX / zoomer.offsetWidth * 100;
+    const y2 = offsetY / zoomer.offsetHeight * 100;
+    zoomer.style.backgroundPosition = x + "% " + y2 + "%";
+  });
+}
+{
   script();
 }
-delegateEvents(["click"]);
-delegateEvents(["click"]);
-delegateEvents(["click", "keydown"]);
 const main = "";
